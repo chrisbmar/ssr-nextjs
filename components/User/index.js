4f9b146e6729891a49caeecec7e5b85373e01cfe
chrisbmar <50221462+chrisbmar@users.noreply.github.com>
2020-04-04 14:32:31 +0200

    scoped styles with styled-jsx

--- components/User/index.js
+++ components/User/index.js
@@ -4,6 +4,15 @@ const User = (props) => (
   <div>
     <h1>{props.name}</h1>
     <p>Age: {props.age}</p>
+    <style jsx>
+      {`
+        div {
+          border: 1px solid #eee;
+          padding: 10px;
+          text-align: center;
+        }
+      `}
+    </style>
   </div>
 );
 
